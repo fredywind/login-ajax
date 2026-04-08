98b1138f836a80fe626adaec0338991ed0cbd6ee
Reza Ananda <reza.anandaputra05@gmail.com>
2026-04-08 08:29:56 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -31,7 +31,7 @@
 
           
           <div class="w3-center">
-            <p>Hello, Selamat Datang. Anda Berhasil Login</p>
+            <p>Hello, Selamat Datang. Anda Berhasil Loginn</p>
             <button class="w3-blue w3-section">LOGOUT</button>
           </div>
 
